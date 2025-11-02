80a3d2f1ff018ad7f42f1e72f9fbff2a2c2063f1
roi biton <roibitonroi@gmail.com>
2025-11-02 15:02:04 +0200

    fix html file

---
+++ js/services/storage-service.js
@@ -0,0 +1,13 @@
+'use strict'
+
+function saveToStorage(key, value) {
+    const json = JSON.stringify(value)
+    localStorage.setItem(key, json)
+}
+
+function loadFromStorage(key) {
+    const json = localStorage.getItem(key)
+    const value = JSON.parse(json)
+    return value
+
+}
